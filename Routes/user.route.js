f8a1c6eac91d0025d8c5b54aa3c58cc67a26c9ff
abdur-rashid-khan <rashidkhan56925@gmail.com>
2022-10-20 10:32:47 +0600

    update 2

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -3,11 +3,11 @@ const userControllers = require('../Controllers/user.controller');
 const router = express.Router();
 
 
-// router.get("/random", userControllers.getRandomUser)
+router.get("/random", userControllers.getRandomUser)
 
-// router.get("/all", userControllers.getAllUser)
+router.get("/all", userControllers.getAllUser)
 
-// router.post("/save", userControllers.saveUser)
+router.post("/save", userControllers.saveUser)
 
 router.patch("/update/:id", userControllers.updateUser)
 router.patch("/bulk-update", userControllers.updateManyUser)
